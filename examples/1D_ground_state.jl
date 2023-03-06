8ed53e5cdbe9c032e4c2ed96e304c9b4cf7aedb6
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-03-06 12:25:16 +0100

    add: change potential width

--- examples/1D_ground_state.jl
+++ examples/1D_ground_state.jl
@@ -27,26 +27,30 @@ x = X[1]
 k = K[1]
 dV= volume_element(L, N)
 reltol = 1e-4
-psi_0 = exp.(-x.^2/5)
+psi_0 = exp.(-(x/16).^2)
 psi_0 = psi_0 / sqrt(ns(psi_0, sim))
 initial_state .= psi_0
 @info "norm squared initial state" ns(psi_0, sim)
 flags = FFTW.EXHAUSTIVE
 kspace!(psi_0, sim)
+
 ## TODO : not getting the potential because of function
-width = 1
-@. V0= 1/2 * ((x/width)^2)
+width = 1/2
+@. V0 = 1/2 * (x^2/(width^2))
 tf = Inf
 @pack_Sim! sim
 
 # Analytical solution: Gaussian
 analytical_gs = zeros(N)
-@. analytical_gs = exp(-((x/width)^2)/2)/(pi^(1/4)*sqrt(width))
-@warn ns(analytical_gs, sim)
+width = sqrt(width)
+@. analytical_gs = exp(-x^2/((width^2)*2)) /(pi^(1/4)*sqrt(width))
 sol = runsim(sim; info=false)
 
 final = sol[end] |> collect
-@info "chemical potential" chempot(final, sim)
+@info ns(analytical_gs, sim)
+@info "chempot of analytical" chempot(analytical_gs, sim)
+
+@info "chemical potential" chempotk(final, sim)
 p = plot(real.(k), abs2.(kspace(initial_state, sim)), color=:blue, ls=:dot, lw=3, label="initial")
 plot!(p, real.(k), abs2.(final), color=:red, label="final")
 plot!(p, real.(k), abs2.(kspace(analytical_gs, sim)), ls=:dot, lw=2, color=:grey, label="analytical")
@@ -62,8 +66,9 @@ middle = Int(round(N[1]/2))
 reduced_x = real.(x)[middle-10:middle+10]
 p = plot(real.(x), abs2.(initial_state), color=:blue, ls=:dot, lw=3, label="initial")
 plot!(p, real.(x), abs2.(final), color=:red, label="final")
-plot!(p, real.(x), abs2.(final)/ns(final, sim), color=:red,  label="final normalized", ls=:dash)
 plot!(p, real.(x), abs2.(analytical_gs), ls=:dot, lw=2, color=:grey, label="analytical")
+#plot!(p, real.(x), abs.(V0), ls=:dash, lw=2, color=:green, label="potential")
+
 display(p)
 
 @warn sum(abs2.(final-analytical_gs))
