36aac18f50436ebc0fccecb49b764c08dd9d2ac0
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-03-14 11:23:38 +0100

    ass: NPSE branches of solver

--- src/solver.jl
+++ src/solver.jl
@@ -2,7 +2,7 @@
 Time evolution in xspace 3D
 """
 function nlin!(dpsi,psi,sim::Sim{3, CuArray{ComplexF64}},t)
-   @unpack ksquared,g,X,V0,iswitch,dV,Vol,mu = sim; x = X[1]; y = X[2]; z = X[3]
+   @unpack g,X,V0,iswitch,dV,Vol,mu = sim; x = X[1]; y = X[2]; z = X[3]
    dpsi .= psi
    mu_im = 0.0
    if iswitch == -im 
@@ -21,9 +21,9 @@ Time evolution in kspace 3D
 function propagate!(dpsi, psi, sim::Sim{3, CuArray{ComplexF64}}, t; info=false)
    @unpack ksquared,iswitch,X,mu,gamma = sim; x = X[1]
    nlin!(dpsi,psi,sim,t)
-   @. dpsi = (1.0 - im*gamma)*(-im*(1/2*ksquared - mu)*psi + dpsi)
-   @info nsk(psi, sim)
-   #@info CUDA.memory_status()
+   #@. dpsi = (1.0 - im*gamma)*(-im*(1/2*ksquared - mu)*psi + dpsi)
+   #@. dpsi += -im*(1/2*ksquared)*psi
+   #@info nsk(psi, sim)
    return nothing
 end
 
@@ -32,14 +32,19 @@ end
 Time evolution in xspace
 """
 function nlin!(dpsi,psi,sim::Sim{1, Array{ComplexF64}},t)
-   @unpack ksquared,g,X,V0,iswitch,dV,Vol,mu = sim; x = X[1]
+   @unpack ksquared,g,X,V0,iswitch,dV,Vol,mu,equation = sim; x = X[1]
    dpsi .= psi
    mu_im = 0.0
    if iswitch == -im
       mu_im = chempotk(psi, sim) # wainting for a more efficient implementation
    end
    xspace!(dpsi,sim)
-   @. dpsi *= -im*iswitch* (V0 + V(x, t) + g*abs2(dpsi)) + mu_im
+   if equation == GPE_1D
+      @. dpsi *= -im*iswitch* (V0 + V(x, t) + g*abs2(dpsi)) + mu_im
+   elseif equation == NPSE
+      @. dpsi *= -im*iswitch* (V0 + V(x, t) + g*abs2(dpsi)/sigma2(dpsi, sim)
+       + (1/(2*sigma2(dpsi, sim)) + 1/2*sigma2(dpsi, sim))) + mu_im
+   end
    kspace!(dpsi,sim)
    return nothing
 end
@@ -49,14 +54,10 @@ end
 Time evolution in kspace
 """
 function propagate!(dpsi, psi, sim::Sim{1, Array{ComplexF64}}, t; info=false)
-   @unpack ksquared, iswitch, equation, dV, Vol,mu,gamma = sim
-   if equation == GPE_1D
+   @unpack ksquared, iswitch, dV, Vol,mu,gamma = sim
       nlin!(dpsi,psi,sim,t)
       #    @. dϕ = -im*(1.0 - im*γ)*(dϕ + (espec - μ)*ϕ)
       @. dpsi = (1.0 - im*gamma)*(-im*(1/2*ksquared - mu)*psi + dpsi)
-   elseif equation == NPSE
-      throw("Unimplemented")
-   end
    return nothing
 end
 
