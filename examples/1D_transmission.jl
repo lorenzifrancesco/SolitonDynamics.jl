7a0beec5de63668628cf31f8e6bc2ddbf424f49d
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-03-27 15:38:09 +0200

    change: script initialization organization

--- examples/1D_transmission.jl
+++ examples/1D_transmission.jl
@@ -18,29 +18,35 @@ sim = Sim{length(L), Array{Complex{Float64}}}(L=L, N=N)
 
 # ======= packing sim
 @unpack_Sim sim
-g = -0.0
-gamma = 0.0
-
-g_param = abs(g) /2
-equation = GPE_1D
+# ======= simulation custom parameters
+equation = NPSE
 solver = SplitStep 
-sigma2 = init_sigma2(g)
+g = -1.17
+gamma = 0.0
+tiles = 8
+barrier_width = 0.699 # as in SolitonBEC.jl
+max_vel = 1.17 # CALCULATED VALUE 1.17 FOR CHOOSEN NONLINEARITY
+max_bar = 1.68 # CALCULATED VALUE 1.68 FOR CHOOSEN NONLINEARITY
+vx = 7
+bx = 7
 
+reltol = 1e-4
+abstol = 1e-4
+
+x0 = L[1] / 4
+
+# other computations
 iswitch = 1
+g_param = abs(g) /2
+sigma2 = init_sigma2(g)
 x = X[1] |> real
 k = K[1] |> real
 dV= volume_element(L, N)
-reltol = 1e-4
-abstol = 1e-4
-x0 = L[1] / 5
 
-# ====== tiling settings 
-tiles = 25
-vel_list = LinRange(0, abs(g), tiles)
-bar_list = LinRange(0, abs(g)^2, tiles)
-
-vv = vel_list[15]
-bb = bar_list[15]
+vel_list = LinRange(0, max_vel, tiles)
+bar_list = LinRange(0, max_bar, tiles)
+vv = vel_list[vx]
+bb = bar_list[bx]
 if vv == 0.0
     tf = 2.0
 else
@@ -49,19 +55,19 @@ end
 Nt = 200
 t = LinRange(ti, tf, Nt)
 
-
 time_steps = 200
 dt = (tf-ti)/time_steps
-maxiters = time_steps * 2
+maxiters = 20000
 
-#@. psi_0 = sqrt(g_param/2) * 2/(exp(g_param*(x-x0)) + exp(-(x-x0)*g_param)) * exp(-im*(x-x0)*vv)
-@. psi_0 = exp(-(x-x0)^2/2)*exp(-im*30*x)
+@. psi_0 = sqrt(g_param/2) * 2/(exp(g_param*(x-x0)) + exp(-(x-x0)*g_param)) * exp(-im*(x-x0)*vv)
+
 psi_0 = psi_0 / sqrt(sum(abs2.(psi_0) * dV))
 initial_state = psi_0
 kspace!(psi_0, sim)
 alg = BS3()
-#@. V0 = bb*exp(-(x/0.699)^2)
-@. V0 = 1/2*x^2
+
+@. V0 = bb*exp(-(x/barrier_width)^2)
+
 @pack_Sim! sim
 
 
