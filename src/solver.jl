e112daf856f8aa7fbea5fdac059bf12e2fa07292
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-03-27 15:48:16 +0200

    change: 3D solver

--- src/solver.jl
+++ src/solver.jl
@@ -1,29 +1,65 @@
+#start old
+# """
+# Time evolution in xspace 3D
+# """
+# function nlin!(dpsi,psi,sim::Sim{3, CuArray{ComplexF64}},t)
+#    @unpack g,X,V0,iswitch,dV,Vol,mu = sim; x = X[1]; y = X[2]; z = X[3]
+#    dpsi .= psi
+#    mu_im = 0.0
+#    if iswitch == -im 
+#       mu_im = chempotk(psi, sim) # wainting for a more efficient implementation
+#    end
+#    xspace!(dpsi, sim)
+#    @. dpsi *= -im * (V0 + V.(x, y, z, t) + g*abs2(dpsi)) + mu_im
+#    kspace!(dpsi, sim)
+#    return nothing
+# end
+
+
+# """
+# Time evolution in kspace 3D 
+# """
+# function propagate!(dpsi, psi, sim::Sim{3, CuArray{ComplexF64}}, t; info=false)
+#    @unpack ksquared,iswitch,X,mu,gamma = sim; x = X[1]
+#    nlin!(dpsi,psi,sim,t)
+#    #@. dpsi = (1.0 - im*gamma)*(-im*(1/2*ksquared - mu)*psi + dpsi)
+#    @. dpsi += -im*(1/2*ksquared)*psi
+#    #@info nsk(psi, sim)
+#    return nothing
+# end
+#end
+
 """
-Time evolution in xspace 3D
+Time evolution in xspace
 """
 function nlin!(dpsi,psi,sim::Sim{3, CuArray{ComplexF64}},t)
-   @unpack g,X,V0,iswitch,dV,Vol,mu = sim; x = X[1]; y = X[2]; z = X[3]
+   @unpack ksquared,g,X,V0,iswitch,dV,Vol,mu,equation,sigma2 = sim; x = X[1]
    dpsi .= psi
    mu_im = 0.0
-   if iswitch == -im 
+   if iswitch == -im
       mu_im = chempotk(psi, sim) # wainting for a more efficient implementation
    end
-   xspace!(dpsi, sim)
-   @. dpsi *= -im * (V0 + V.(x, y, z, t) + g*abs2(dpsi)) + mu_im
-   kspace!(dpsi, sim)
+   xspace!(dpsi,sim)
+   if equation == GPE_1D
+      @. dpsi *= -im*iswitch* (V0 + V(x, t) + g*abs2(dpsi)) + mu_im
+   elseif equation == NPSE
+      nonlinear = g*abs2.(dpsi) ./sigma2.(dpsi) + (1 ./(2*sigma2.(dpsi)) + 1/2*sigma2.(dpsi))
+      @. dpsi *= -im*iswitch* (V0 + V(x, y, z, t) + nonlinear) + mu_im
+      #@info g*maximum(abs2.(dpsi))
+   end
+   kspace!(dpsi,sim)
    return nothing
 end
 
 
 """
-Time evolution in kspace 3D 
+Time evolution in kspace
 """
 function propagate!(dpsi, psi, sim::Sim{3, CuArray{ComplexF64}}, t; info=false)
-   @unpack ksquared,iswitch,X,mu,gamma = sim; x = X[1]
-   nlin!(dpsi,psi,sim,t)
-   #@. dpsi = (1.0 - im*gamma)*(-im*(1/2*ksquared - mu)*psi + dpsi)
-   @. dpsi += -im*(1/2*ksquared)*psi
-   #@info nsk(psi, sim)
+   @unpack ksquared, iswitch, dV, Vol,mu,gamma = sim
+      nlin!(dpsi,psi,sim,t)
+      #    @. dϕ = -im*(1.0 - im*γ)*(dϕ + (espec - μ)*ϕ)
+      @. dpsi = (1.0 - im*gamma)*(-im*(1/2*ksquared - mu)*psi + dpsi)
    return nothing
 end
 
@@ -119,7 +155,7 @@ Imaginary time evolution in kspace
 function ground_state_evolve!(psi, sim::Sim{1, Array{ComplexF64}}, dt; info=false)
       @unpack ksquared,g,X,V0,iswitch,dV,Vol = sim; x = X[1]
       kspace!(psi, sim)
-      @. psi += -dt/2*(1/2 *ksquared) * psi
+      @. psi += -dt/2*(1/2 * ksquared) * psi
       xspace!(psi, sim)
    return nothing
 end
@@ -197,6 +233,7 @@ using BKW Euler
 """
 function be_ground_state!(psi,sim::Sim{3, CuArray{ComplexF64}}, dt, tri_fwd, tri_bkw; info=false)
    @unpack dt,g,X,V0,iswitch,dV,Vol,N = sim; x = X[1]
+   throw("Broken")
    psi_i = copy(psi) 
    nonlin = -(dt/2) * g*abs2.(psi)
    tri_bkw += Diagonal(nonlin)
