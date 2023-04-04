12ad87c404d42602eddfca009d8656ee9448b704
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-04-04 10:09:53 +0200

    add: manual 3D routines

--- src/solvers_3D_manual.jl
+++ src/solvers_3D_manual.jl
@@ -1,5 +1,23 @@
 # Particular solvers to be used manually
 
+# ============== ManualSplitStep methods, improved with exp
+
+function nlin_manual!(dpsi,psi,sim::Sim{3, CuArray{ComplexF64}},t)
+   @unpack ksquared,g,X,V0,dV,Vol,mu,equation,sigma2,dt,iswitch = sim; x = X[1]
+   xspace!(psi,sim)
+   @. psi = exp(dt/2 * -im*iswitch* (V0 + V(x,y,z,t) + 2*pi*g*abs2(psi))) * psi
+   kspace!(psi,sim)
+   return nothing
+end
+
+function propagate_manual!(dpsi, psi, sim::Sim{3, CuArray{ComplexF64}}, t; info=false)
+   @unpack ksquared, iswitch, dV, Vol,mu,gamma,dt = sim
+   nlin_manual!(dpsi,psi,sim,t)
+   @. psi = exp(dt/2 * (1.0 - im*gamma)*(-im*(1/2*ksquared - mu)))*psi
+   return nothing
+end
+
+# ============== Manual BE GS
 
 """
 3D Imaginary time evolution in xspace, 
