5ed6c36e327dc3155ad059f4f192a416aa7e4c14
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-04-04 10:09:04 +0200

    fix: 3D auto solver

--- src/solvers_3D_auto.jl
+++ src/solvers_3D_auto.jl
@@ -8,13 +8,7 @@ function nlin!(dpsi,psi,sim::Sim{3, CuArray{ComplexF64}},t)
       mu_im = chempotk(psi, sim) # wainting for a more efficient implementation
    end
    xspace!(dpsi,sim)
-   if equation == GPE_1D
-      @. dpsi *= -im*iswitch* (V0 + V(x, t) + g*abs2(dpsi)) + mu_im
-   elseif equation == NPSE
-      nonlinear = g*abs2.(dpsi) ./sigma2.(dpsi) + (1 ./(2*sigma2.(dpsi)) + 1/2*sigma2.(dpsi))
-      @. dpsi *= -im*iswitch* (V0 + V(x, y, z, t) + nonlinear) + mu_im
-      #@info g*maximum(abs2.(dpsi))
-   end
+   @. dpsi *= -im*iswitch* (V0 + V(x,y,z,t) + 2*pi*g*abs2(dpsi)) + mu_im
    kspace!(dpsi,sim)
    return nothing
 end
@@ -23,7 +17,6 @@ end
 function propagate!(dpsi, psi, sim::Sim{3, CuArray{ComplexF64}}, t; info=false)
    @unpack ksquared, iswitch, dV, Vol,mu,gamma = sim
       nlin!(dpsi,psi,sim,t)
-      #    @. dϕ = -im*(1.0 - im*γ)*(dϕ + (espec - μ)*ϕ)
       @. dpsi = (1.0 - im*gamma)*(-im*(1/2*ksquared - mu)*psi + dpsi)
    return nothing
 end
