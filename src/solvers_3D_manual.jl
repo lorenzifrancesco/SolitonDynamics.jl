f36bab2feeaf8c4f04d7fdc1aebb7f337fa0589b
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-04-05 11:14:50 +0200

    fix: manual evolution has half velocity for 3D

--- src/solvers_3D_manual.jl
+++ src/solvers_3D_manual.jl
@@ -5,7 +5,7 @@
 function nlin_manual!(dpsi,psi,sim::Sim{3, CuArray{ComplexF64}},t)
    @unpack ksquared,g,X,V0,dV,Vol,mu,equation,sigma2,dt,iswitch = sim; x = X[1]; y = X[1]; z = X[1]
    xspace!(psi,sim)
-   @. psi = exp(dt/2 * -im*iswitch* (V0 + V(x,y,z,t) + 2*pi*g*abs2(psi))) * psi
+   @. psi = exp(dt * -im*iswitch* (V0 + V(x,y,z,t) + 2*pi*g*abs2(psi))) * psi
    kspace!(psi,sim)
    return nothing
 end
@@ -13,7 +13,7 @@ end
 function propagate_manual!(dpsi, psi, sim::Sim{3, CuArray{ComplexF64}}, t; info=false)
    @unpack ksquared, iswitch, dV, Vol,mu,gamma,dt = sim
    nlin_manual!(dpsi,psi,sim,t)
-   @. psi = exp(dt/2 * (1.0 - im*gamma)*(-im*(1/2*ksquared - mu)))*psi
+   @. psi = exp(dt * (1.0 - im*gamma)*(-im*(1/2*ksquared - mu)))*psi
    return nothing
 end
 
