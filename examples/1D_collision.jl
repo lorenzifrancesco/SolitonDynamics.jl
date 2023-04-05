df0253f1b6519d03804f9177774f5c008732341b
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-04-05 10:44:32 +0200

    change: pulse propagation to soliton

--- examples/1D_collision.jl
+++ examples/1D_collision.jl
@@ -17,17 +17,25 @@ N = (256,)
 sim = Sim{length(L), Array{Complex{Float64}}}(L=L, N=N)
 
 @unpack_Sim sim
-g = -3.0
+g = -2.5
+g_param = abs(g) / 2
+
 equation = GPE_1D
+manual = true
+Nt = 100
+t = LinRange(ti,tf,Nt)
+
 iswitch = 1
 x = X[1]
 k = K[1]
-dV= volume_element(L, N)
+dV = volume_element(L, N)
 reltol = 1e-4
-tf = 10.0
-x0 = -10
-@. psi_0 = exp(-(x-x0)^2/2) * exp(im*x*2)
 
+x0 = L[1]/4
+vv = 6.0
+bb = 
+tf = x0*2/vv
+@. psi_0 = sqrt(g_param/2) * 2/(exp(g_param*(x-x0)) + exp(-(x-x0)*g_param)) * exp(-im*(x-x0)*vv)
 psi_0 = psi_0 / sqrt(sum(abs2.(psi_0) * dV))
 initial_state = psi_0
 kspace!(psi_0, sim)
@@ -36,9 +44,8 @@ alg = Tsit5()
 
 @pack_Sim! sim
 
-
 sol = runsim(sim; info=false)
-final = sol[end]
+final = sol.u[end]
 xspace!(final, sim)
 xspace!(psi_0, sim)
 @info "final distribution norm squared: " ns(final, sim)
@@ -48,7 +55,7 @@ plot!(p, real.(x), abs2.(final), label="final")
 display(p)
 xspace!(initial_state, sim)
 
-
-map(x -> xspace!(x, sim), sol[:])
-ht = heatmap(real.(x), sol.t, abs2.(sol)')
+map(x -> xspace!(x, sim), sol.u[:])
+u_mat = mapreduce(permutedims, hcat, sol.u)
+ht = heatmap(real.(x), sol.t, abs2.(u_mat)')
 display(ht)
