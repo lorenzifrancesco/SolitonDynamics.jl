c68b44f700e8f8793b12605ccca98b1b0d234811
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-04-06 13:24:06 +0200

    fix: solution handling

--- examples/3D_tiles.jl
+++ examples/3D_tiles.jl
@@ -11,57 +11,8 @@ import Makie, GLMakie
 using ProgressBars
 
 JULIA_CUDA_SOFT_MEMORY_LIMIT ="95%"
-# ================ plotting functions
-function dense(phi)
-    psi = xspace(phi,sim)
-    density = abs2.(psi)
-    pmax = maximum(density)
-    if pmax == 0
-        throw("Maximum density is null")
-    end
-    return density/pmax
-end
-
-function isosurface_animation(sol,Nt, sim;file="3Dcollision.gif", framerate=3)
-    saveto=joinpath("media",file)
-    scene = Makie.Scene()
-    tindex = Makie.Observable(1)
-    iter = [Array(xspace(sol[k], sim)) for k in 1:Nt]
-    iter = [abs2.(iter[k]) for k in 1:Nt]
-    scene = Makie.volume(Makie.@lift(iter[$tindex]/maximum(iter[$tindex])),
-                        algorithm =:iso,
-                        isovalue=0.2,
-                        isorange=0.1,
-                        colormap=:greens, 
-                        alpha=0.3,
-                        transparency=true
-    )
-
-    R = 180
-    eyeat = Makie.Vec3f0(R,0,0)
-    lookat = Makie.Vec3f0(-50,-50,0)
-    Makie.record(scene, saveto, 1:Nt; framerate=framerate) do i
-        # Makie.update_cam!(scene, eyeat, lookat)
-        # Makie.rotate_cam!(scene, 0., -0.4, 0.)
-        tindex[] = i
-    end
-    return
-end
-
-function isosurface(sol)
-    scene = Makie.Scene()
-    tindex = Makie.Observable(1)
-    psol = Array(abs2.(xspace(sol, sim)))
-    scene = Makie.volume(psol/maximum(psol),
-                        algorithm =:iso,
-                        colormap=:greens, 
-                        alpha=0.3,
-                        isovalue=0.2,
-                        isorange=0.1,
-    )
-    display(scene)
-    return
-end
+include("plot_isosurfaces.jl")
+include("plot_axial_evolution.jl")
 
 file = "3Dtran.pdf"
 let sim
@@ -92,22 +43,12 @@ dV= volume_element(L, N)
 
 t = LinRange(ti,tf,Nt)
 # nfiles = true
-maxiters = 5000
+maxiters = 10000
 x0 = L[1]/4
 
-# tmp = [(exp(-(y^2+z^2)/2) * sqrt(g_param/2)*2/(exp(g_param*(x-x0)) + exp(-(x-x0)*g_param))) * exp(-im*x*vv) for x in x, y in y, z in z]
-tmp = [exp(-(y^2+z^2+(x-x0)^2)/2) * exp(-im*x*vv) for x in x, y in y, z in z]
-
-psi_0 = CuArray(tmp)
-
-psi_0 .= psi_0 / sqrt(sum(abs2.(psi_0) * dV))
-initial_state = psi_0
 kspace!(psi_0, sim)
 alg = BS3()
-#1/2*(x^2+y^2+ 3*z^2)
-tmp = [ (1/2*(y^2+ z^2) + 0.0*1/2*x^2 + 400*exp(-10*x^2)) for x in x, y in y, z in z]
-V0 = CuArray(tmp)
-#V(x,y,z,t) = 1/2 * (x^2+y^2+z^2)
+
 @pack_Sim! sim
 
 
@@ -153,7 +94,7 @@ for ((vx, vv), (bx, bb)) in ProgressBar(iter)
     
     @info "Running solver..."
     sol = runsim(sim; info=false)
-    final = sol[:, :, :, end]
+    final = sol.u[end]
     @info "Run complete, computing transmission..."
     xspace!(final, sim)
     tran[bx, vx] = ns(final, sim, mask_tran)
