f2e9855b17a511c4d2496d049b02908ee45b65cb
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-04-11 14:40:15 +0200

    fix: overwriting when transforming

--- examples/plot_axial_evolution.jl
+++ examples/plot_axial_evolution.jl
@@ -18,9 +18,9 @@ function plot_axial_heatmap(u, time_axis, sim::Sim{3, CuArray{ComplexF64}}, axis
     ax_list = (1, 2, 3)
     ax_list= filter(x->x!=axis, ax_list)
 
-    doifft ? [xspace!(x, sim) for x in u] : nothing
+    doifft ? ux = [xspace(x, sim) for x in u] : nothing
     # SPECIALIZE to axis = 3
-    u_axial = [sum(abs2.(x), dims=ax_list)[1,1,:] for x in u]
+    u_axial = [sum(abs2.(x), dims=ax_list)[1,1,:] for x in ux]
     u_axial = Array(reduce(hcat, u_axial))
     ht = heatmap(real.(x), time_axis, u_axial')
     display(ht)
@@ -29,11 +29,11 @@ end
 
 function plot_final_density(u, psi_0, sim::Sim{1, Array{ComplexF64}}; info=false, doifft=true)
     @unpack t, X = sim; x = Array(X[1])
-    final = u[end]
-    doifft ? xspace!(final, sim) : nothing
-    doifft ? xspace!(psi_0, sim) : nothing
+    tmp = u[end]
+    doifft ? final = xspace(tmp, sim) : nothing
+    doifft ? psi = xspace(psi_0, sim) : nothing
     info && @info "final norm" ns(final, sim)
-    p = plot(real.(x), abs2.(psi_0), label="initial")
+    p = plot(real.(x), abs2.(psi), label="initial")
     plot!(p, real.(x), abs2.(final), label="final")
     display(p)
     return p
@@ -47,8 +47,8 @@ function plot_final_density(u, psi_0, sim::Sim{3, CuArray{ComplexF64}}, axis; in
     doifft ? final = xspace(final, sim) : nothing
     info && @info "final norm" ns(final, sim)
     final_axial = Array(sum(abs2.(final), dims=ax_list))[1,1,:]
-    doifft ? xspace!(psi_0, sim) : nothing
-    psi_0_axial = Array(sum(abs2.(psi_0), dims=ax_list))[1,1,:]
+    doifft ? psi = xspace(psi_0, sim) : nothing
+    psi_0_axial = Array(sum(abs2.(psi), dims=ax_list))[1,1,:]
     p = plot(real.(x), psi_0_axial, label="initial")
     plot!(p, real.(x), final_axial, label="final")
     display(p)
@@ -57,13 +57,17 @@ end
 
 function animation_final_density(u,sim::Sim{1, Array{ComplexF64}};file="1D_evolution.gif",framerate=30,info=false, doifft=true)
     @unpack t, X, Nt = sim; x = Array(X[1]) |> real
+    # override until next solution 
+    Nt = length(u)
     saveto=joinpath("media",file)
     tindex = Makie.Observable(1)
+    iter = u
     doifft ? iter = [Array(xspace(u[k], sim)) for k in 1:Nt] : nothing
     #iter = [ for k in 1:Nt]
     iter = [abs2.(iter[k]) for k in 1:Nt]
+    fig, ax= Makie.lines(x, Makie.@lift(iter[$tindex]))
 
-    fig = Makie.lines(x, Makie.@lift(iter[$tindex]))
+    Makie.limits!(ax, x[1], x[end], 0, 1)
     Makie.record(fig, saveto, 1:Nt; framerate=framerate) do i
         tindex[] = i
     end
