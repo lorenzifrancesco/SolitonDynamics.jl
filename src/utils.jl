4583bc0c5aed137f593a7af5857e45aac3883595
lorenzi-on-cloudveneto <francesco.lorenzi.2@studenti.unipd.it>
2023-05-10 15:46:24 +0000

    fix: estimate_sigma2

--- src/utils.jl
+++ src/utils.jl
@@ -1,9 +1,10 @@
 """
 estimate the Gaussian sigma2 parameter from 3D data
 """
-function estimate_sigma2(psi,sim::Sim{3, CuArray{ComplexF64}})
+function estimate_sigma2(psi_k,sim::Sim{3, CuArray{ComplexF64}})
     s2 = Array{Float64, 1}(undef, sim.N[1])
     # MSE estimator
+    psi = xspace(psi_k, sim)
     dx = sim.X[1][2] - sim.X[1][1]
     aa = Array(abs2.(psi))
     xax = 1:sim.N[1]
@@ -11,16 +12,14 @@ function estimate_sigma2(psi,sim::Sim{3, CuArray{ComplexF64}})
     zax = 1:sim.N[3]
     tmp = zeros(sim.N)
     axial_density = sum(aa, dims=(2, 3))[:, 1, 1] * sim.dV
-    @info sum(axial_density * dx)
-    @info size(axial_density)
     for x in xax
         for y in yax
             for z in zax
-                tmp[x, y, z] = sim.X[2][y]^2 * sim.X[3][z]^2 * aa[x, y, z] / axial_density[x] * sim.dV
+                tmp[x, y, z] = sim.X[2][y] * sim.X[3][z] * aa[x, y, z] / axial_density[x] * sim.dV
             end
         end
     end
-    @info size(tmp)
+
     s2 = sum(tmp, dims=(2, 3))[:, 1, 1]
     return s2
 end
