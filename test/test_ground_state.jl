4027feac1c849c3f1538a37f471cb515e9318f84
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-06-10 14:44:34 +0200

    fix: test using manual GS routine (auto is broken)

--- test/test_ground_state.jl
+++ test/test_ground_state.jl
@@ -3,7 +3,7 @@ using CondensateDynamics
 ## Solve the 1D harmonic oscillator
 # problem with 1D-GPE 
 L = (40.0,)
-N = (256,)
+N = (512,)
 sim = Sim{length(L), Array{Complex{Float64}}}(L=L, N=N)
 
 @unpack_Sim sim
@@ -11,13 +11,17 @@ sim = Sim{length(L), Array{Complex{Float64}}}(L=L, N=N)
 iswitch=-im
 g = 0.0
 equation = GPE_1D
+manual = true
+abstol = 1e-7
 x = X[1]
 dV= volume_element(L, N)
 psi_0 = exp.(-x.^2/5)
 psi_0 = psi_0 / sqrt(ns(psi_0, sim))
 
 kspace!(psi_0, sim)
+
 @. V0= 1/2 * (x^2)
+
 @pack_Sim! sim
 
 # Analytical solution: Gaussian
@@ -26,6 +30,5 @@ analytical_gs = zeros(N)
 
 sol, err = testsim(sim)
 @test err == false
-numerical_gs = xspace(sol[1], sim)
-
+numerical_gs = xspace(sol.u, sim)
 @test isapprox(ns((numerical_gs-analytical_gs), sim), 0.0, atol=1e-5)
