3fa2ab30aa2211aa179dba3ad7d517f1ea0fd5b6
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2023-06-26 09:28:40 +0200

    add: tiling and tiling prompt

--- research/plotter_all.jl
+++ research/plotter_all.jl
@@ -7,7 +7,6 @@ function sdmf()
         @info "running in multi-thread mode: n_threads =" Threads.nthreads()
     end
 
-
     ## procedure:
     # 1- compute ground states
     # 2- prepare dynamical simulations using GS
@@ -16,15 +15,17 @@ function sdmf()
     save_path = "results/"
     gamma_list = [0.15, 0.4, 0.6]
     for gamma in gamma_list
+        @info "==== Using gamma: " gamma
         sd = load_parameters(gamma_param=gamma)
-        @info "Using gamma: " gamma
         @info "Required simulations: " keys(sd)
         # prepare ground states (saving them)
         if isfile(save_path * "gs_dict.jld2")
             @info "Loading GS library..."
             gs_dict = JLD2.load(save_path * "gs_dict.jld2")
         else
-            @info "No GS library found!"
+            @info "No GS library found! Saving an empty one..."
+            gs_dict = Dict()
+            JLD2.save(save_path * "gs_dict.jld2", gs_dict)
         end
 
         # preparing all simulations
@@ -39,9 +40,18 @@ function sdmf()
             end
         end
 
-        # visualize the ground states
-        # TODO
 
+        # visualize the ground states
+        print("\nProceed to tiling? [Y/n]: ")
+        if readline()[1] == 'n'
+            @info "Aborting..."
+            return
+        end
         # run the tiling
+        for (name, sim) in sd
+            @info "Tiling " name
+            tile = get_tiles(sim, name)
+            JLD2.@save(save_path * "tile_dict.jld2", hs(name, gamma) => tile)
+        end
     end
 end
